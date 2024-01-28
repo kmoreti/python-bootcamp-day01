cecb689c58ff3d9a7d4942198185966f109269c4
kmoreti <kleber.moreti.camargo@gmail.com>
2024-01-28 23:04:40 +0000

    Strings, input, variables

---
+++ day-1-printing-start/mayin.py
@@ -0,0 +1,16 @@
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Kleber")
+# input() will get user input in console
+# Then print() will print the word "Hello"  and the user input
+print("Hello " + input("What is your name?"))
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
